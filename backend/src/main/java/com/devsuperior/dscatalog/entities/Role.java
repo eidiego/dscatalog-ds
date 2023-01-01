ef0c5b5c28410e5f06e8f11e1bef2cc6ed255ba4
Diego Rodrigues Barbosa <diegokod@gmail.com>
2022-12-31 23:27:29 -0300

    Feat: Add Entities Role and User

---
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Role.java
@@ -0,0 +1,34 @@
+package com.devsuperior.dscatalog.entities;
+
+public class Role {
+	
+	private Long id;
+	private String authority;
+	
+	public Role() {
+	}
+
+	public Role(Long id, String authority) {
+		super();
+		this.id = id;
+		this.authority = authority;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getAuthority() {
+		return authority;
+	}
+
+	public void setAuthority(String authority) {
+		this.authority = authority;
+	}
+	
+
+}
